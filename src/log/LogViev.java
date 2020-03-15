ca672cc80b94184d999c1f39a9bdc59e7a21910e
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2020-03-15 11:07:08 +0100

    Logger vorbereitet

---
+++ src/log/LogViev.java
@@ -0,0 +1,14 @@
+package log;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class LogViev extends JPanel {
+   /**
+    * Create the panel.
+    */
+   public LogViev() {
+      setLayout(new BorderLayout());
+   }
+}
---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author andreas kielkopf
+ *
+ */
+module tasmoView {
+   exports de.uhingen.kielkopf.andreas.tasmoview.grafik;
+   exports de.uhingen.kielkopf.andreas.tasmoview.sensors;
+   exports de.uhingen.kielkopf.andreas.tasmoview.minijson;
+   exports de.uhingen.kielkopf.andreas.tasmoview;
+   requires transitive java.desktop;
+   requires transitive java.prefs;
+   requires java.net.http;
+}
